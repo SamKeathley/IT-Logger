ce5ba40dfc3db913e37c8ddb202f060cbc905b1d
Sam Keathley <samncollette@gmail.com>
2020-11-10 15:15:10 -0800

    search logs functionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   SET_CURRENT,
   CLEAR_CURRENT,
-  UPDATE_LOG
+  UPDATE_LOG,
+  SEARCH_LOGS
 } from './types';
 
 // Get logs from server
@@ -103,6 +104,26 @@ export const updateLog = (log) => async (dispatch) => {
   }
 };
 
+// Search server for logs
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
 // Set current log
 export const setCurrent = (log) => {
   return {
@@ -111,7 +132,7 @@ export const setCurrent = (log) => {
   };
 };
 
-// clear current log
+// Clear current log
 export const clearCurrent = () => {
   return {
     type: CLEAR_CURRENT
